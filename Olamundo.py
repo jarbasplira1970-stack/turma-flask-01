e5ff68d55ba461f5c1210f232995e55b087f8662
JARBAS LIRA <jarbasplira1970@gmail.com>
2026-05-05 21:06:07 -0300

    Exemplo de condicional

--- Olamundo.py
+++ Olamundo.py
@@ -1,4 +1,11 @@
 nome = input('Dgite o nome: ')
 
+idade = int(input("digite"))
+
 print(f'Ola, {nome}!')
 
+if idade >= 18:
+    print("vc é maior de idade")
+else:
+    print("vc pe menor de idade")
+    
